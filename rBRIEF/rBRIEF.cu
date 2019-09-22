60f5696df33e137f80a29005c503b8c18945d77c
minhsqtruong <minhsqtruong@gmail.com>
2019-09-21 23:22:47 -0400

    cpu compiled and run

--- rBRIEF/rBRIEF.cu
+++ rBRIEF/rBRIEF.cu
@@ -2,5 +2,37 @@
 
 // write implementation of rBRIEF module here
 
+
+void cpu_oBRIEF(float sin_theta,
+                float cos_theta,
+                float* patch,
+                int patch_dim,
+                int * pattern,
+                bool * binary_feature
+                )
+{
+
+  int ax, ay, bx, by;
+  int rotated_ax, rotated_ay, rotated_bx, rotated_by;
+  int Ia, Ib;
+
+  for (int i = 0; i < 256; ++i) {
+    ax = pattern[4*i];
+    ay = pattern[4*i+1];
+    bx = pattern[4*i+2];
+    by = pattern[4*i+3];
+
+    rotated_ax = (int) (cos_theta * ax - sin_theta * ay);
+    rotated_ay = (int) (sin_theta * ay + cos_theta * ay);
+    rotated_bx = (int) (cos_theta * bx - sin_theta * by);
+    rotated_by = (int) (sin_theta * by + cos_theta * by);
+
+    Ia = patch[rotated_ax + patch_dim * rotated_ay];
+    Ib = patch[rotated_bx + patch_dim * rotated_by];
+
+    binary_feature[i] = Ia > Ib;
+  }
+};
+
 // test pipeline integration
 void pipeline_print_rBRIEF(){ printf("rBRIEF Module active!\n");};
--- rBRIEF/tb_rBRIEF.cu
+++ rBRIEF/tb_rBRIEF.cu
@@ -1,9 +1,31 @@
 #include"rBRIEF.cuh"
+#include<stdio.h>
 int main(int argc, char const *argv[]) {
 
   // write test code for rBRIEF module here
+  float sin_theta = 0.9;
+  float cos_theta = 0.3;
+  float patch [100];
+  int patch_dim = 10;
+  int * pattern = cpu_precompute_BRIEF_pattern;
+  bool binary_feature[256];
 
+  // initialize patch
+  for (int i = 0; i < 100; i++) {
+    patch[i] = (float) i;
+  }
+  cpu_oBRIEF(sin_theta,
+            cos_theta,
+            patch,
+            patch_dim,
+            pattern,
+            binary_feature);
+
+  // print binary_feature
+  for (int i = 0; i < 256; i++) {
+    printf("%d\n", binary_feature[i]);
+  }
   // test pipeline integration
-  pipeline_print_rBRIEF();
+  //pipeline_print_rBRIEF();
   return 0;
 }
