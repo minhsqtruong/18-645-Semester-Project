c43a4c562d7321510f349f85c9b9d5686befa348
minhsqtruong <minhsqtruong@gmail.com>
2019-09-30 11:21:41 -0400

    Add compiled benchmark

--- app/gpu_benchmark.cu
+++ app/gpu_benchmark.cu
@@ -1,17 +1,16 @@
 /*
 Benchmark GPU on the following operation:
-Arithmetic: + - * / % 
+Arithmetic: + - * / %
 Single Precision Intrinsics: __cosf __sinf __fadd_rd __fdiv_rd __fmad_rd __fmul_rd
-Integer Intrinsics: __hadd __mul24 __sad 
+Integer Intrinsics: __hadd __mul24 __sad
 */
 
-#define STREAM_LENGTH 1000
 #include<cuda.h>
-
+#include<stdio.h>
 /*=============================================================
 		INTEGER ARITHMETIC LATENCY KERNEL
 =============================================================*/
-__global__ 
+__global__
 void i_add_latency()
 {
 	long long unsigned start;
@@ -19,15 +18,15 @@ void i_add_latency()
 	int c = 0;
 	int a = 1;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c+=a;
 	end = clock64();
 	printf("Integer add latency: %llu\n", end - start);
-	
+
 }
 
-__global__ 
+__global__
 void i_min_latency()
 {
 	long long unsigned start;
@@ -35,15 +34,15 @@ void i_min_latency()
 	int c = 0;
 	int a = 1;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c-=a;
 	end = clock64();
 	printf("Integer minus latency: %llu\n", end - start);
-	
+
 }
 
-__global__ 
+__global__
 void i_mul_latency()
 {
 	long long unsigned start;
@@ -51,15 +50,15 @@ void i_mul_latency()
 	int c = 1;
 	int a = 1;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c*=a;
 	end = clock64();
 	printf("Integer multiply latency: %llu\n", end - start);
-	
+
 }
 
-__global__ 
+__global__
 void i_mod_latency()
 {
 	long long unsigned start;
@@ -67,18 +66,18 @@ void i_mod_latency()
 	int c = 0;
 	int a = 1;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c%=a;
 	end = clock64();
 	printf("Integer modulo latency: %llu\n", end - start);
-	
+
 }
 
 /*=============================================================
 	    SINGLE PRECISION ARITHMETIC LATENCY KERNEL
 =============================================================*/
-__global__ 
+__global__
 void f_add_latency()
 {
 	long long unsigned start;
@@ -86,15 +85,15 @@ void f_add_latency()
 	float c = 0.0;
 	float a = 1.0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c+=a;
 	end = clock64();
 	printf("Single Precision add latency: %llu\n", end - start);
-	
+
 }
 
-__global__ 
+__global__
 void f_min_latency()
 {
 	long long unsigned start;
@@ -102,15 +101,15 @@ void f_min_latency()
 	float c = 0.0;
 	float a = 1.0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c-=a;
 	end = clock64();
 	printf("Single Precision minus latency: %llu\n", end - start);
-	
+
 }
 
-__global__ 
+__global__
 void f_mul_latency()
 {
 	long long unsigned start;
@@ -118,15 +117,15 @@ void f_mul_latency()
 	float c = 1.0;
 	float a = 1.0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c*=a;
 	end = clock64();
 	printf("Single Precision multiply latency: %llu\n", end - start);
-	
+
 }
 
-__global__ 
+__global__
 void f_div_latency()
 {
 	long long unsigned start;
@@ -134,27 +133,27 @@ void f_div_latency()
 	float c = 1.0;
 	float a = 2.0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c/=a;
 	end = clock64();
 	printf("Single Precision divide latency: %llu\n", end - start);
-	
+
 }
 
 /*=============================================================
 	       INTEGER INTRINSICS LATENCY KERNEL
 =============================================================*/
 __global__
-void hadd_latency()
+void had_latency()
 {
 	long long unsigned start;
 	long long unsigned end;
 	int c = 10000;
 	int a = 10000;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c = __hadd(a,c);
 	end = clock64();
 	printf("Integer Intrinsic average latency: %llu\n", end - start);
@@ -168,24 +167,24 @@ void mul24_latency()
 	int c = 1;
 	int a = 1;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c = __mul24(a,c);
 	end = clock64();
 	printf("Integer Intrisic multiply latency: %llu\n", end - start);
 };
 
 __global__
-void sadd_latency()
+void sad_latency()
 {
 	long long unsigned start;
 	long long unsigned end;
 	int c = 1;
 	int a = 1;
-	int b = 0'
+	int b = 0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c = __sad(a,c,b);
 	end = clock64();
 	printf("Interger Intrinsic absolute value latency: %llu\n", end - start);
@@ -203,8 +202,8 @@ void cosf_latency()
 	long long unsigned end;
 	float c = 1.0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c = __cosf(c);
 	end = clock64();
 	printf("Single Precision Intrinsic cosine latency: %llu\n", end - start);
@@ -217,8 +216,8 @@ void sinf_latency()
 	long long unsigned end;
 	float c = 1.0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
 		c = __sinf(c);
 	end = clock64();
 	printf("Single Precision Intrinsic sine latency: %llu\n", end - start);
@@ -230,11 +229,11 @@ void fadd_rd_latency()
 	long long unsigned start;
 	long long unsigned end;
 	float c = 1.0;
-	float a = 1.0
+	float a = 1.0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
-		c = fadd_rd(a,c);
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
+		c = __fadd_rd(a,c);
 	end = clock64();
 	printf("Single Precision Intrinsic add latency: %llu\n", end - start);
 };
@@ -245,11 +244,11 @@ void fdiv_rd_latency()
 	long long unsigned start;
 	long long unsigned end;
 	float c = 1.0;
-	float a = 1.0
+	float a = 1.0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
-		c = fdiv_rd(a,c);
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
+		c = __fdiv_rd(a,c);
 	end = clock64();
 	printf("Single Precision Intrinsic divide latency: %llu\n", end - start);
 };
@@ -260,12 +259,12 @@ void fmaf_rd_latency()
 	long long unsigned start;
 	long long unsigned end;
 	float c = 1.0;
-	float a = 1.0
+	float a = 1.0;
 	float b = 0.0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
-		c = fmaf_rd(a,b,c);
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
+		c = __fmaf_rd(a,b,c);
 	end = clock64();
 	printf("Single Precision Intrinsic FMA latency: %llu\n", end - start);
 };
@@ -276,18 +275,18 @@ void fmul_rd_latency()
 	long long unsigned start;
 	long long unsigned end;
 	float c = 1.0;
-	float a = 1.0
+	float a = 1.0;
 	start = clock64();
-	#pragma unroll (STREAM_LENGTH)
-	for (int i = 0 ; i < STREAM_LENGTH; i++)
-		c = fmul_rd(a,c);
+	#pragma unroll (1000)
+	for (int i = 0 ; i < 1000; i++)
+		c = __fmul_rd(a,c);
 	end = clock64();
 	printf("Single Precision Intrinsic multiply latency: %llu\n", end - start);
 };
 
 
 
-int main(void) 
+int main(void)
 {
 
 	/*
@@ -303,6 +302,15 @@ int main(void)
 	cudaDeviceSynchronize();
 	i_mod_latency<<<1,1>>>();
 	cudaDeviceSynchronize();
+	
+	// Integer Intrinsics
+	had_latency<<<1,1>>>();
+	cudaDeviceSynchronize();
+	mul24_latency<<<1,1>>>();
+	cudaDeviceSynchronize();
+	sad_latency<<<1,1>>>();
+	cudaDeviceSynchronize();
+
 
 	//Single precision Arithmetic
 	f_add_latency<<<1,1>>>();
@@ -312,14 +320,6 @@ int main(void)
 	f_mul_latency<<<1,1>>>();
 	cudaDeviceSynchronize();
 	f_div_latency<<<1,1>>>();
-	cudaDeviceSynchronize();
-
-	// Integer Intrinsics
-	hadd__latency<<<1,1>>>();
-	cudaDeviceSynchronize();
-	mul24_latency<<<1,1>>>();
-	cudaDeviceSynchronize();
-	sad_latency<<<1,1>>>();
 	cudaDeviceSynchronize();
 
 	//Single precision Intrinsics;
