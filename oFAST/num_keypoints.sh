a3c267835732fa881f59bdf6b92f777fe6b26943
Frank Lee <popofrank97@gmail.com>
2019-12-06 23:11:49 -0500

    everything works

--- oFAST/num_keypoints.sh
+++
@@ -1,3 +0,0 @@
-for i in {10..300..10}
-do nvprof --unified-memory-profiling off ./a.out $i >> threshold.txt
-done 
--- oFAST/threshold.py
+++
@@ -1,8 +0,0 @@
-import os
-for run in range(10,30,10):
-    os.system("/usr/local/cuda/bin/nvprof --log-file tmp.txt --unified-memory-profiling off ./a.out %d" %(run))
-    with open('./threshold.txt') as log:
-        for line in log:
-            if ("calcKeyPoints" in line):
-                val = float(line.split()[1][:-2])
-                print(run, val)
